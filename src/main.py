7dc0612c89d932b4ac160cdc6fa94f087d7bcb5d
Hannah Woodbridge <hannah.woodbridge@student.isae-supaero.fr>
2018-12-12 10:35:05 +0100

    quick fix of directory

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import config
 from src.processing.preprocessing import excel_to_df
 from src.searches.exact_search import find_exact_matches, count_exact_matches
-from src.searches.classification import classify
+# from src.searches.classification import classify
 
 
 def main():
@@ -12,7 +12,6 @@ def main():
     term = sys.argv[1]
     match_df = find_exact_matches(term, data_df)
     print(count_exact_matches(term, match_df), match_df)
-    print(classify(data_df, sys.argv[2]))
 
 
 if __name__ == '__main__':

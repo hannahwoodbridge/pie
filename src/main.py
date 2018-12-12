f32181147801739d28c9be18b119c3c3ab00e9b8
matthieuhastings <matthieuhts@gmail.com>
2018-12-12 16:06:56 +0100

    Added output functionnality

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import config
 from src.processing.preprocessing import excel_to_df
 from src.searches.exact_search import find_exact_matches, count_exact_matches
+from src.output.output import df_to_excel
 # from src.searches.classification import classify
 
 
@@ -11,7 +12,9 @@ def main():
     data_df = excel_to_df(file_name)
     term = sys.argv[1]
     match_df = find_exact_matches(term, data_df)
+    df_to_excel(match_df,term)
     print(count_exact_matches(term, match_df), match_df)
+    print("Le fichier output est à l'adresse ./output")
 
 
 if __name__ == '__main__':

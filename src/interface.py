c35f67758bffe31545bd771dd3de0928b8a2b566
matthieuhastings <matthieuhts@gmail.com>
2019-01-16 13:34:04 +0100

    Date choice functionnality, main using argParse

--- src/interface.py
+++ src/interface.py
@@ -1,9 +1,13 @@
 import tkinter as tk 
+import tkcalendar
+from tkinter import simpledialog
+
 
 class MainWindow(tk.Frame):
     def __init__(self, master, OPTIONS):
         self.master=master
-        self.label = tk.Label(master, text="Enter a term to search in the database")
+        self.label = tk.Label(master, 
+                              text="Enter a term to search in the database")
         self.label.pack()
         
         #Setting term and search parameters initial values
@@ -19,6 +23,15 @@ class MainWindow(tk.Frame):
         args = (master, self.search_type) + tuple(OPTIONS)
         self.search_list = tk.OptionMenu(*args)
         self.search_list.pack()
+        
+        #Creation of selection dates       
+        button = tk.Button(master, text="Selectionnez la premiere date", 
+                           command=self.onclick_date1)
+        button.pack()
+               
+        button = tk.Button(master, text="Selectionnez la seconde date", 
+                           command=self.onclick_date2)
+        button.pack()
         
         #Validation or cancelation buttons
         frame = tk.Frame(master)
@@ -43,8 +56,23 @@ class MainWindow(tk.Frame):
             raise ValueError('Please select a type of search')
         self.master.destroy()
         
+    def onclick_date1 (self):
+        cd = CalendarDialog(self.master)
+        self.date1=cd.result
         
+    def onclick_date2 (self):
+        cd = CalendarDialog(self.master)
+        self.date2=cd.result
         
+
+class CalendarDialog(simpledialog.Dialog):
+    """Dialog box that displays a calendar and returns the selected date"""
+    def body(self, master):
+        self.calendar = tkcalendar.Calendar(master)
+        self.calendar.pack()
+
+    def apply(self):
+        self.result = self.calendar.get_date()      
    
 
 def get_search_data():
@@ -55,9 +83,9 @@ def get_search_data():
     """
     #List of search options
     OPTIONS = [
-        "Exact_search",
-        "CASSE_search",
-        "Search_by_family"
+        "exact_search",
+        "exact_search case_sensitivity",
+        "synonyms_search"
     ]
     
     # Creation of window interface
@@ -66,5 +94,59 @@ def get_search_data():
     window = MainWindow(root,OPTIONS)
     root.mainloop()
     
-    return window.term, window.search_type
+    return window.term, window.search_type, window.date1, window.date2
 
+########## Displaying results #########
+       
+class DispWindow(tk.Frame):
+    """
+    Class defining the window object that displays the result of the search
+    """
+    def __init__(self, master):
+        self.master=master
+        
+        #Displaying number of events found
+        frame1=tk.Frame(master)
+        frame1.pack()
+        self.var_nb_event=tk.StringVar()
+        self.var_nb_event.set("")
+        self.text_event=tk.Label(frame1, text="Number of events found:")
+        self.event=tk.Label(frame1, textvariable=self.var_nb_event)
+        self.text_event.pack(side=tk.LEFT)
+        self.event.pack(side=tk.RIGHT)
+        
+        #Displaying time for calculation
+        frame2=tk.Frame(master)
+        frame2.pack()
+        self.var_time=tk.StringVar()
+        self.var_time.set("")
+        self.text_time=tk.Label(frame2, text="Time for calculation:")
+        self.time=tk.Label(frame2, textvariable=self.var_time)
+        self.text_time.pack(side=tk.LEFT)
+        self.time.pack(side=tk.RIGHT)
+        
+        #Validation button
+        self.OK = tk.Button(master, text="OK", command=master.destroy)
+        self.OK.pack()
+
+    def update(self,nb_events, time):
+        """
+        A class method to update the labels in the window
+        :param nb_events: number of events found
+        :param time: time of calculation
+        """
+        self.var_nb_event.set(str(nb_events))
+        self.var_time.set(str(time))
+        #self.master.update_idletasks()
+
+def disp_search_result(nb_events, time, term, search_type):
+    """
+    A box that displays the number of events and time of calculation
+    
+    """
+    
+    root=tk.Tk()
+    root.title(f"Searching {term} with option {search_type}")
+    window = DispWindow(root)
+    window.update(nb_events,time)
+    root.mainloop()
--- src/main.py
+++ src/main.py
@@ -5,43 +5,29 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import config
 from src.processing.preprocessing import preprocess
 from src.searches.exact_search import find_exact_matches, count_exact_matches
-<<<<<<< HEAD
-from src.output.output import df_to_excel
-from src.interface import get_search_data
-=======
+from src.interface import get_search_data, disp_search_result
 from src.output.output import write_output
+import time
 
->>>>>>> master
 
-def exact_search(term, case_sensitive):
+def exact_search(term, case_sensitive=False):
     file_name_short = config.CROPS['file_name']
     file_name = f"{file_name_short}.xlsx"
+    start=time.time()                       
     df = preprocess(file_name)
-
-<<<<<<< HEAD
-    #term = unidecode(sys.argv[1].lower())
-    #term = get_term()
-    term,search_type = get_search_data()
-    print(term)
-    print(search_type)
-    if search_type=="Exact_search":
-        match_df = find_exact_matches(term, df)
-        df_to_excel(match_df,term)
-    
-        print(f"Exact matches found: {count_exact_matches(term, match_df)}")
-        print("Output file is in folder ./output")
-    else:
-        print(f"{search_type} not implemented yet")
-=======
     match_df = find_exact_matches(term, df, case_sensitive)
     write_output(match_df, term, file_name_short)
+    end = time.time()
+
+    diff_time = end - start
+    disp_search_result(count_exact_matches(term, match_df,case_sensitive), 
+                       diff_time, term, search_type)
 
     print(
         f"Exact matches found: "
         f"{count_exact_matches(term, match_df,case_sensitive)} \n"
         f"Output file is in folder ./output"
     )
->>>>>>> master
 
 
 
@@ -74,3 +60,16 @@ if __name__ == '__main__':
 
         case_sensitive = arguments.cs
         exact_search(term, case_sensitive)
+    
+    ##### Use of interface instead of passing arguments #######
+    if  arguments.action == 'interface':
+        term,search_type,date_1,date_2 = get_search_data()
+        if search_type=="exact_search":
+            exact_search(term)
+        elif search_type=="exact_search case_sensitivity":
+            case_sensitive = True
+            exact_search(term, case_sensitive)
+        else:
+            print(f"{search_type} not implemented yet")
+            quit()
+            
